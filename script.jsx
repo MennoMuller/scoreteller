55152b5038b7f25fbfc2a19244db241e4efc55ce
MennoMuller <menno.muller.nl@gmail.com>
2023-01-12 10:57:29 +0100

    made score a state

--- script.jsx
+++ script.jsx
@@ -1,14 +1,16 @@
 const players = [
-  { name: "Menno", score: 200, id: 1 },
-  { name: "Kees", score: 250, id: 2 },
-  { name: "Piet", score: 100, id: 3 }
+  { name: "Menno", id: 1 },
+  { name: "Kees", id: 2 },
+  { name: "Piet", id: 3 }
 ];
 
 const Header = (props) => {
   return (
     <header id="test">
       <h1>{props.title}</h1>
-      <span className="stats">Players: {props.totalPlayers}</span>
+      <span className="stats">
+        Players: {props.totalPlayers}
+      </span>
     </header>
   );
 };
@@ -17,32 +19,60 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">{props.name}</span>
-      <Counter score={props.score} />
+      <Counter />
     </div>
   );
 };
 
-const Counter = (props) => {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement">-</button>
-      <span className="counter-score">{props.score}</span>
-      <button className="counter-action increment">+</button>
-    </div>
-  );
-};
+// const Counter = (props) => {
+//   return (
+//     <div className="counter">
+//       <button className="counter-action decrement">-</button>
+//       <span className="counter-score">{props.score}</span>
+//       <button className="counter-action increment">+</button>
+//     </div>
+//   );
+// };
+
+class Counter extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     score: 0
+  //   };
+  // }
+  state = { score: 0 };
+
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement">
+          -
+        </button>
+        <span className="counter-score">
+          {this.state.score}
+        </span>
+        <button className="counter-action increment">
+          +
+        </button>
+      </div>
+    );
+  }
+}
 
 const App = () => {
   return (
     <div className="scoreboard">
-      <Header title="Scoreboard" totalPlayers={players.length} />
+      <Header
+        title="Scoreboard"
+        totalPlayers={players.length}
+      />
 
       {/* Player list */}
 
       {players.map((player) => (
         <Player
           name={player.name}
-          score={player.score}
           key={player.id.toString()}
         />
       ))}
@@ -50,5 +80,7 @@ const App = () => {
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 root.render(<App />);
